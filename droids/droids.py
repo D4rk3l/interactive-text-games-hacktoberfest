e82d3c1e94e8659297939c3e1edb7e8ffa746dc7
Charith Jayasanka <charithj@zone24x7.com>
2019-10-22 16:53:11 +0530

    droids finalized

--- droids/droids.py
+++ droids/droids.py
@@ -283,3 +283,236 @@ will have to hack the data recorder.\n''')
 			print('....')
 			time.sleep(2)
 			print('10000101010101010101010' * 1000)
+			time.sleep(1)
+			print('....')
+			time.sleep(1)
+			print('Accessing encrypted files...')
+			time.sleep(2)
+			print('Decrypting....')
+			time.sleep(2)
+			print('\n\n[-SEN343 LOG-]')
+			time.sleep(1)
+			print('\n\nDAILY OVER-RIDE CODES- HANGAR DROIDS')
+			time.sleep(1)
+			print('\n\n-Combat Droids - szb41ee')
+			time.sleep(1)
+			print('\n\n-Sentry Droids - qr66mop')
+			time.sleep(1)
+			print('\n\n-Repair Droids - yu8xxj3')
+			time.sleep(1)
+			print('\n\nCODES WILL BE RESET EVERY 24 HOURS')
+			security(inventory)
+		else:
+			print('\n- ACCESS TO DATA RECORDER DENIED -')
+			time.sleep(2)
+			security(inventory)
+		
+	elif cmd == '3':
+		start(inventory)
+		
+def observation(inventory):
+	print('\n----------')
+	print('\nDroid mobile..')
+	time.sleep(1)
+	print('....')
+	time.sleep(1)
+	print('''\nYou enter the Observation deck and are confronted 
+with a Enemy Sentry droid beside a disabled crew droid. 
+His laser is almost charged and will be active in seconds.\n''')
+	print('[-OBSERVATION-]\n')
+	print('1.) Terminate Sentry Droid')
+	print('2.) Retreat to Main Elevator.')
+	
+	cmdlist = ['1', '2']
+	cmd = getcmd(cmdlist)
+	
+	if cmd == '1':
+		if 'laser' in inventory:
+			print('\nlaser active...')
+			time.sleep(1)
+			print('target locked...')
+			time.sleep(1)
+			print('...')
+			time.sleep(1)
+			print('\nTARGET TERMINATED\n')
+			enemy_sentry(inventory)
+		else:
+			print('\n- WARNING LASER OFFLINE -')
+			time.sleep(2)
+			print('''\nThe Sentry Droids laser is now active and has you locked
+on. You try to initiate self-destruct but its to late..''')
+			print('....')
+			time.sleep(1)
+			print('....')
+			time.sleep(1)
+			print('\nshutdown imminent...')
+			time.sleep(1)
+			print('CTRL866 offline.')
+			time.sleep(1)
+			print('Droid terminated.')
+			print('\nGAME OVER\n')
+			exit(0)
+			
+	elif cmd == '2':
+			print('''\nThe Sentry droids laser is now active and has you locked
+on. You try to retreat back to the elevator but its to late..''')
+			print('....')
+			time.sleep(1)
+			print('....')
+			time.sleep(1)
+			print('\nshutdown imminent...')
+			time.sleep(1)
+			print('CTRL866 offline.')
+			time.sleep(1)
+			print('Droid terminated.')
+			print('\nGAME OVER\n')
+			exit(0)
+			
+def enemy_sentry(inventory, items=['motion tracker']):
+	print('\n----------')
+	time.sleep(1)
+	print('''\nThe Enemy Sentry droid has been terminated. 
+Judging by the model you know he has a
+motion tracker repair program installed. You MUST connect 
+to this droid with service port and download the program.''')
+	if len(items) > 0:
+		for item in items:
+			print('\n--> %s' % (item))
+	cmdlist = ['service port']
+	cmd = getcmd(cmdlist)
+	if cmd == 'service port':
+			inventory.append('motion tracker')
+			items = ['motion tracker']
+			print('\nservice port connected.')
+			time.sleep(1)
+			print('accessing file..')
+			time.sleep(1)
+			print('downloading..')
+			time.sleep(1)
+			print('....')
+			time.sleep(1)
+			print('Repairing Motion Tracker...')
+			time.sleep(1)
+			print('Auto alignment...')
+			time.sleep(1)
+			print('....')
+			time.sleep(2)
+			print('\nMOTION TRACKER ONLINE.')
+			time.sleep(2)
+			print('''\nYour Motion Tracker is now online.
+You return to the main elevator''')
+			start(inventory)
+			
+def shuttle_control(inventory):
+	print('\n----------')
+	print('\nDroid mobile..')
+	time.sleep(1)
+	print('....')
+	time.sleep(1)
+	print('''\nYou enter Shuttle Control where all navigation takes place.
+A 999 Elite Enemy Command droid is posted here.
+This Droid is extremely powerfull.''')
+	print('\n[-SHUTTLE CONTROL-]')
+	print('\n1.) Terminate the 999 Elite Enemy Command Droid')
+	print('2.) Retreat to Main Elevator')
+	cmdlist = ['1', '2']
+	cmd = getcmd(cmdlist)
+	
+	if cmd == '1':
+		if 'laser' in inventory and 'motion tracker' in inventory and 'droid hack' in inventory:
+			print('\n....')
+			time.sleep(1)
+			print('\n....')
+			command_droid(inventory)
+		else:
+			time.sleep(1)
+			print('\nEECD999:>')
+			print('\n100101010101010101010101010101010' * 10)
+			time.sleep(1)
+			print('''\nThe Elite Enemy Command droid laughs in machine language
+at your pathetic attempt. The last thing your data recorder gets is the
+deafing sound of a Target Lock.''')
+			print('....')
+			time.sleep(1)
+			print('....')
+			time.sleep(1)
+			print('\nshutdown imminent...')
+			time.sleep(1)
+			print('CTRL866 offline.')
+			time.sleep(1)
+			print('Droid terminated.')
+			print('\nGAME OVER\n')
+			exit(0)
+	elif cmd == '2':
+		start(inventory)
+		
+def command_droid(inventory):
+	print('\nRunning droid hack...')
+	time.sleep(1)
+	print('\njamming EECD999 Target Lock...')
+	time.sleep(1)
+	print('\n......')
+	time.sleep(1)
+	print('\nMotion Tracker active...')
+	time.sleep(1)
+	print('\nTrack motion of EECD999...')
+	time.sleep(1)
+	print('\n......')
+	time.sleep(1)
+	print('\nLaser active...')
+	time.sleep(1)
+	print('\nTargeting EECD999...')
+	time.sleep(1)
+	print('\nTarget Locked...')
+	time.sleep(1)
+	print('\n......')
+	time.sleep(2)
+	print('\n\nTARGET TERMINATED \n')
+	time.sleep(2)
+	print('''\n\nYou have defeated the EECD999 droid and taken back control
+of the 'KERNEL'. The flight path has been restored and
+a distress signal sent to Droid Command. Reinforcements are inbound.
+\n - GAME OVER -\n''')
+
+def getcmd(cmdlist):
+	cmd = input('\nCTRL866:> ')
+	if cmd in cmdlist:
+		return cmd
+	elif cmd == 'help':
+		print('\nTYPE: inventory to view items')
+		print('or quit to self-destruct')
+		return getcmd(cmdlist)
+	elif cmd == 'inventory':
+		print('\ninventory contains:\n')
+		for item in inventory:
+			print('-- %s' % (item))
+		return getcmd(cmdlist)
+	elif cmd == 'secret':
+		print('\n........')
+		time.sleep(1)
+		print('\n[--Paradroid -- published by Hewson 1985--]')
+		time.sleep(1)
+		print('\n[--written by Andrew Braybrook for Commodore 64 computer--]')
+		time.sleep(1)
+		print('\n[--play this game or die--]')
+		time.sleep(1)
+		print('\n........\n')
+		return getcmd(cmdlist)
+	elif cmd == 'quit':
+		print('\n----------')
+		time.sleep(1)
+		print('\nself-destruct sequence initiated...')
+		time.sleep(1)
+		print('shutdown imminent...')
+		time.sleep(1)
+		print('\nCTRL866 offline.')
+		time.sleep(1)
+		print('Droid terminated.\n')
+		exit(0)
+	else:
+		print('\n   error. invalid command-\n')
+		return getcmd(cmdlist)
+	
+if __name__ == "__main__":
+	inventory = ['service port']
+	start(inventory)
