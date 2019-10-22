23e39d8cc07fe273746d16252699c7e324695fb8
Charith Jayasanka <charithj@zone24x7.com>
2019-10-22 16:50:41 +0530

    text-base-forest-game.py added

---
+++ text-base-forest-game/text-base-forest-game.py
@@ -0,0 +1,43 @@
+# Setup
+yes_no = ["yes", "no"]
+directions = ["left", "right", "forward", "backward"]
+ 
+# Introduction
+name = input("What is your name, adventurer?\n")
+print("Greetings, " + name + ". Let us go on a quest!")
+print("You find yourself on the edge of a dark forest.")
+print("Can you find your way through?\n")
+ 
+# Start of game
+response = ""
+while response not in yes_no:
+    response = input("Would you like to step into the forest?\nyes/no\n")
+    if response == "yes":
+        print("You head into the forest. You hear crows cawwing in the distance.\n")
+    elif response == "no":
+        print("You are not ready for this quest. Goodbye, " + name + ".")
+        quit()
+    else: 
+        print("I didn't understand that.\n")
+ 
+# Next part of game
+response = ""
+while response not in directions:
+    print("To your left, you see a bear.")
+    print("To your right, there is more forest.")
+    print("There is a rock wall directly in front of you.")
+    print("Behind you is the forest exit.\n")
+    response = input("What direction do you move?\nleft/right/forward/backward\n")
+    if response == "left":
+        print("The bear eats you. Farewell, " + name + ".")
+        quit()
+    elif response == "right":
+        print("You head deeper into the forest.\n")
+    elif response == "forward":
+        print("You cannot scale the wall.\n")
+        response = "" 
+    elif response == "backward":
+        print("You leave the forest. Goodbye, " + name + ".")
+        quit()
+    else:
+        print("I didn't understand that.\n")
